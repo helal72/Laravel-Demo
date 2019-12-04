fed7cb037e87abbe409f9cd02b4c9ce2974e1571
helal72 <helal.cse.pust72@gmail.com>
2019-12-04 06:59:14 +0600

    RelationShip

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -2,16 +2,16 @@
 @extends('backend.layouts.app')
 @section('content')
   <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
 
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </p>
 
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
  </div>
 @endsection
